49bc7554db02e5e5ecc9e6c315a7b9f50a9bc092
SajawalHassan <scientificninja1f12@gmail.com>
2021-07-29 15:44:19 +0500

    Completed the header

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Home from "./Home.js";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>SearchPage</h1>
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
